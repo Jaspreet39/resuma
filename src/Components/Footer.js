ccb3502fe3976e6fe87006fc370ac9cabec8fe9c
jaspreet39 <jassingh0244@gmail.com>
2021-06-07 10:46:19 +0530

    resuma built

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,15 +18,15 @@ const Footer = ({ data }) => {
       <div className="row">
         <div className="twelve columns">
           <ul className="social-links">{networks}</ul>
-
+{/* 
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
+              <a title="Jaspreet" href="http://www.papareact.com/">
                 PAPA
               </a>
             </li>
-          </ul>
+          </ul> */}
         </div>
         <div id="go-top">
           <a className="smoothscroll" title="Back to Top" href="#home">
